c3e1819fb3464601aac719a0d13b4c38cda4c439
HanugaFathurC <hanugafc@gmail.com>
2023-04-22 21:18:30 +0700

    Doc: add initial project with login-register

---
+++ inbox/app/Models/TransactionDetail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TransactionStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'quantity', 'durasi', 'tagihan', 'status', 'transaction_id', 'product_id'
+    ];
+
+    protected $casts = [
+        'status' => TransactionStatus::class
+    ];
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public function transaction(){
+        return $this->belongsTo(Transaction::class);
+    }
+
+
+}
---
+++ inbox/resources/views/auth/login.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.auth.master', ['title' => 'Login - Inbox'])
+
+@section('content')
+    <div class="w-full  bg-white rounded-lg shadow-lg border mx-auto sm:max-w-md  md:-mt-32 px-6 py-12">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company">
+            <h1 class="text-xl font-bold text-center text-gray-900 ">
+                INBOX
+            </h1>
+        </div>
+
+        <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="{{ route('login') }}" method="POST">
+                @csrf
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" autocomplete="email"
+                            placeholder="Masukan email mu" required
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-1.5 ">
+                        @error('email')
+                            <div class="text-sm text-red-600 mt-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <div class="mt-2">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required
+                            placeholder="Masukkan kata sandi"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-1.5 ">
+                        @error('password')
+                            <div class="text-sm text-red-600 mt-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Masuk</button>
+                </div>
+            </form>
+
+            <p class="mt-10 text-center text-sm text-gray-500">
+                Belum punya akun?
+                <a href="{{ route('register') }}"
+                    class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Daftar</a>
+            </p>
+        </div>
+    </div>
+@endsection
